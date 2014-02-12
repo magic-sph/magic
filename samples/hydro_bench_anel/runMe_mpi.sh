ba3e4c7d8de314cb41929525dd5224d00cafd441
Tilman Dannert <tilman.dannert@ipp.mpg.de>
2014-02-12 16:54:41 +0000

    Pure MPI version, successful testsuite.

--- samples/hydro_bench_anel/runMe_mpi.sh
+++ samples/hydro_bench_anel/runMe_mpi.sh
@@ -8,7 +8,13 @@ export KMP_AFFINITY=noverbose,granularity=core,scatter
 sed "s/nThreadsRun *= *[0-9]*/nThreadsRun = 1/" input.nml >input_omp1.nml
 
 max_procs=`cat /proc/cpuinfo | grep processor | wc -l`
-echo "Starting magic with ${max_procs} MPI processes"
-mpiexec -n ${max_procs} ./magic.exe input_omp1.nml
+if [ "$max_procs" -ge 16 ]; then
+    n_procs=16
+elif [ "$max_procs" -ge 8 ]; then
+    n_procs=8
+fi
+
+echo "Starting magic with ${n_procs} MPI processes"
+mpiexec -n ${n_procs} ./magic.exe input_omp1.nml
 
 rm input_omp1.nml
