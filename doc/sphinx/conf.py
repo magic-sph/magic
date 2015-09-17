e88706481bdb30ca5600b5fdafeff9e43175d7e2
Thomas Gastine <gastine@mps.mpg.de>
2015-09-17 18:44:25 +0200

    add homepage + html theme

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -116,7 +116,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'classic' # 'sphinx_rtd_theme'
+html_theme = 'magic' # 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -124,7 +124,7 @@ html_theme = 'classic' # 'sphinx_rtd_theme'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = ['.themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -165,7 +165,7 @@ html_static_path = ['.static']
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
-#html_additional_pages = {}
+html_additional_pages = {'home': 'home.html'}
 
 # If false, no module index is generated.
 #html_domain_indices = True
---
+++ doc/sphinx/download.rst
@@ -0,0 +1,4 @@
+:orphan:
+
+
+Download it...
