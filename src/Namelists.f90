e82c90f7fc64ab460f9586c737ebce8e71945c4a
rraynaud <raphael.raynaud@cea.fr>
2018-10-22 14:37:30 +0200

    Do not fix the PNS diffusivity profile according to interior_model
    nVarVisc and nVarDiff can be choosen indendently in input.nml

--- src/Namelists.f90
+++ src/Namelists.f90
@@ -525,16 +525,16 @@ contains
          ! Proto-Neutron Star interior model
          ! at time 0.2s
          l_anel=.true.
-         nVarVisc=3
-         nVarDiff=6
+         ! nVarVisc=3
+         ! nVarDiff=6
          radratio    = 0.5D0
          r_cut_model = 1.0D0
       else if ( index(interior_model, 'PNS_1S') /= 0 ) then
          ! Proto-Neutron Star interior model
          ! at time 1s
          l_anel=.true.
-         nVarVisc=3
-         nVarDiff=6
+         !nVarVisc=3
+         !nVarDiff=6
          radratio    = 0.60D0
          r_cut_model = 1.0D0
       else if ( index(interior_model, 'PNS_5S') /= 0 ) then
