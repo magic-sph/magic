7b8d8ffbf1cf235afac5fd9847bef229c16d2de5
Nathanaël Schaeffer <nathanael.schaeffer@gmail.com>
2019-04-05 00:00:26 +0200

    Improve installation of shtns by bin/install-shtns.sh (with and without OpenMP) and detection by cmake.

--- bin/install-shtns.sh
+++ bin/install-shtns.sh
@@ -1,26 +1,36 @@
 #!/bin/sh
 
-if test ! -d $HOME/local; then
-    mkdir $HOME/local
+if [[ -n $MAGIC_HOME ]]
+then
+  cd $MAGIC_HOME/bin
+else
+  echo "error, please set MAGIC_HOME first (e.g. by running 'source sourceme.sh')"
+  exit 1
 fi
 
 if command -v hg; then
   hg clone https://bitbucket.org/nschaeff/shtns
-  cd shtns
 else
   wget https://bitbucket.org/nschaeff/shtns/downloads/shtns-3.0-r618.tar.gz
   tar -xvf shtns-3.0-r618.tar.gz
   rm shtns-3.0-r618.tar.gz
-  cd shtns-3.0-r618
+  mv shtns-3.0-r618 shtns
 fi
 
+opts="--enable-magic-layout"
 if [[ -n $MKLROOT ]]
 then
    echo "MKL found, installing with MKL"
-   ./configure --prefix=$HOME/local --enable-openmp --enable-magic-layout --enable-mkl
+   opts="$opts -enable-mkl"
 else
    echo "MKL not found, will try to install with FFTW"
-   ./configure --prefix=$HOME/local --enable-openmp --enable-magic-layout
 fi
 
-make install -j
+cd shtns
+## compile without OpenMP
+./configure $opts
+make -j
+## compile with OpenMP
+./configure --enable-openmp $opts
+make -j
+
