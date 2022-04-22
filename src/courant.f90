d59a9dd68d65009814cfa3cd84a6ec9eeeef429d
Thomas Gastine <gastine@ipgp.fr>
2022-04-22 15:25:42 +0200

    add a batched variant for Courant factor

--- src/courant.f90
+++ src/courant.f90
@@ -7,7 +7,7 @@ module courant_mod
 
    use parallel_mod
    use precision_mod
-   use truncation, only: n_phi_max, n_theta_max
+   use truncation, only: n_phi_max, n_theta_max, nlat_padded
    use radial_data, only: nRstart, nRstop
    use radial_functions, only: orho1, orho2, or4, or2
    use physical_parameters, only: LFfac, opm
@@ -23,7 +23,8 @@ module courant_mod
 
    integer :: file_handle
 
-   public :: courant, dt_courant, initialize_courant, finalize_courant
+   public :: courant, dt_courant, initialize_courant, finalize_courant, &
+   &         courant_batch
 
 contains
 
@@ -86,15 +87,9 @@ contains
       real(cp), intent(inout) :: dthkc    ! Courant step based on horizontal advection
 
       !-- Local  variables:
-      integer :: n_theta       ! absolut no of theta
-      integer :: n_theta_nhs   ! no of theta in NHS
-      integer :: n_phi         ! no of longitude
-
-      real(cp) :: valri2,valhi2,valh2,valh2m
-      real(cp) :: vr2max,vh2max
-      real(cp) :: valr,valr2,vflr2,vflh2
-      real(cp) :: O_r_E_2,O_r_E_4
-      real(cp) :: cf2,af2
+      integer :: n_theta, n_theta_nhs, n_phi
+      real(cp) :: valri2,valhi2,valh2,valh2m,vr2max,vh2max
+      real(cp) :: valr,valr2,vflr2,vflh2,cf2,af2
 
 #if (XSH_COURANT==1)
       real(cp) :: dx2_ua, dr2, dh2, dh2n, vflr2max, vflh2max, valr2max, valh2max
@@ -105,8 +100,6 @@ contains
       vflh2max=0.0_cp
       valh2max=0.0_cp
       cf2=courfac*courfac
-      O_r_E_4=or4(n_r)
-      O_r_E_2=or2(n_r)
 
       if ( l_mag .and. l_mag_LF .and. .not. l_mag_kin ) then
 
@@ -124,8 +117,8 @@ contains
                valr =br(n_theta,n_phi)*br(n_theta,n_phi) * &
                &     LFfac*orho1(n_r)
                valr2=valr*valr/(valr+valri2)
-               vflr2max=max(vflr2max,O_r_e_4*cf2*vflr2)
-               valr2max=max(valr2max,O_r_e_4*af2*valr2)
+               vflr2max=max(vflr2max,or4(n_r)*cf2*vflr2)
+               valr2max=max(valr2max,or4(n_r)*af2*valr2)
 
 
                vflh2= ( vt(n_theta,n_phi)*vt(n_theta,n_phi) +  &
@@ -135,8 +128,8 @@ contains
                &        bp(n_theta,n_phi)*bp(n_theta,n_phi) )* &
                &        LFfac*osn2(n_theta_nhs)*orho1(n_r)
                valh2m=valh2*valh2/(valh2+valhi2)
-               vflh2max=max(vflh2max,O_r_E_2*cf2*vflh2)
-               valh2max=max(valh2max,O_r_E_2*af2*valh2)
+               vflh2max=max(vflh2max,or2(n_r)*cf2*vflh2)
+               valh2max=max(valh2max,or2(n_r)*af2*valh2)
             end do
          end do
          !$omp end parallel do
@@ -182,12 +175,12 @@ contains
                n_theta_nhs=(n_theta+1)/2 ! northern hemisphere=odd n_theta
 
                vflr2=orho2(n_r)*vr(n_theta,n_phi)*vr(n_theta,n_phi)
-               vflr2max=max(vflr2max,cf2*O_r_E_4*vflr2)
+               vflr2max=max(vflr2max,cf2*or4(n_r)*vflr2)
 
                vflh2= ( vt(n_theta,n_phi)*vt(n_theta,n_phi) +  &
                &        vp(n_theta,n_phi)*vp(n_theta,n_phi) )* &
                &        osn2(n_theta_nhs)*orho2(n_r)
-               vflh2max=max(vflh2max,cf2*O_r_E_2*vflh2)
+               vflh2max=max(vflh2max,cf2*or2(n_r)*vflh2)
             end do
          end do
          !$omp end parallel do
@@ -223,8 +216,6 @@ contains
       vr2max=0.0_cp
       vh2max=0.0_cp
       cf2=courfac*courfac
-      O_r_E_4=or4(n_r)
-      O_r_E_2=or2(n_r)
 
       if ( l_mag .and. l_mag_LF .and. .not. l_mag_kin ) then
 
@@ -241,7 +232,7 @@ contains
                vflr2=orho2(n_r)*vr(n_theta,n_phi)*vr(n_theta,n_phi)
                valr =br(n_theta,n_phi)*br(n_theta,n_phi)*LFfac*orho1(n_r)
                valr2=valr*valr/(valr+valri2)
-               vr2max=max(vr2max,O_r_e_4*(cf2*vflr2+af2*valr2))
+               vr2max=max(vr2max,or4(n_r)*(cf2*vflr2+af2*valr2))
 
                vflh2= ( vt(n_theta,n_phi)*vt(n_theta,n_phi) +  &
                &        vp(n_theta,n_phi)*vp(n_theta,n_phi) )* &
@@ -250,7 +241,7 @@ contains
                &        bp(n_theta,n_phi)*bp(n_theta,n_phi) )* &
                &        LFfac*osn2(n_theta_nhs)*orho1(n_r)
                valh2m=valh2*valh2/(valh2+valhi2)
-               vh2max=max(vh2max,O_r_E_2*(cf2*vflh2+af2*valh2m))
+               vh2max=max(vh2max,or2(n_r)*(cf2*vflh2+af2*valh2m))
 
             end do
          end do
@@ -266,12 +257,12 @@ contains
                n_theta_nhs=(n_theta+1)/2 ! northern hemisphere=odd n_theta
 
                vflr2=orho2(n_r)*vr(n_theta,n_phi)*vr(n_theta,n_phi)
-               vr2max=max(vr2max,cf2*O_r_E_4*vflr2)
+               vr2max=max(vr2max,cf2*or4(n_r)*vflr2)
 
                vflh2= ( vt(n_theta,n_phi)*vt(n_theta,n_phi) +  &
                &        vp(n_theta,n_phi)*vp(n_theta,n_phi) )* &
                &        osn2(n_theta_nhs)*orho2(n_r)
-               vh2max=max(vh2max,cf2*O_r_E_2*vflh2)
+               vh2max=max(vh2max,cf2*or2(n_r)*vflh2)
 
             end do
 
@@ -286,6 +277,125 @@ contains
 #endif
 
    end subroutine courant
+!------------------------------------------------------------------------------
+   subroutine courant_batch(n_r_l,n_r_u,dtrkc,dthkc,vr,vt,vp,br,bt,bp,courfac,alffac)
+      !
+      !  courant condition check: calculates Courant
+      !  advection lengths in radial direction dtrkc
+      !  and in horizontal direction dthkc
+      !  on the local radial level n_r
+      !
+      !  for the effective velocity, the abs. sum of fluid
+      !  velocity and Alfven velocity is taken
+      !
+      !  instead of the full Alfven velocity
+      !  a modified Alfven velocity is employed that takes
+      !  viscous and Joule damping into account. Different
+      !  Courant factors are used for the fluid velocity and
+      !  the such modified Alfven velocity
+      !
+
+      !-- Input variable:
+      integer,  intent(in) :: n_r_l       ! Starting radial level
+      integer,  intent(in) :: n_r_u       ! Stopping radial level
+      real(cp), intent(in) :: vr(nlat_padded,n_r_l:n_r_u,*)   ! radial velocity
+      real(cp), intent(in) :: vt(nlat_padded,n_r_l:n_r_u,*)   ! longitudinal velocity
+      real(cp), intent(in) :: vp(nlat_padded,n_r_l:n_r_u,*)   ! azimuthal velocity
+      real(cp), intent(in) :: br(nlat_padded,n_r_l:n_r_u,*)   ! radial magnetic field
+      real(cp), intent(in) :: bt(nlat_padded,n_r_l:n_r_u,*)   ! longitudinal magnetic field
+      real(cp), intent(in) :: bp(nlat_padded,n_r_l:n_r_u,*)   ! azimuthal magnetic field
+      real(cp), intent(in) :: courfac
+      real(cp), intent(in) :: alffac
+
+      !-- Output:
+      real(cp), intent(inout) :: dtrkc(n_r_l:n_r_u)  ! Courant step (radial)
+      real(cp), intent(inout) :: dthkc(n_r_l:n_r_u)  ! Courant step (horizontal)
+
+      !-- Local  variables:
+      integer :: n_theta, n_phi, n_theta_nhs, n_r
+      real(cp) :: valri2,valhi2,valh2,valh2m
+      real(cp) :: vr2max(n_r_l:n_r_u),vh2max(n_r_l:n_r_u)
+      real(cp) :: valr,valr2,vflr2,vflh2,cf2,af2
+
+      vr2max=0.0_cp
+      vh2max=0.0_cp
+      cf2=courfac*courfac
+
+      if ( l_mag .and. l_mag_LF .and. .not. l_mag_kin ) then
+
+         af2=alffac*alffac
+
+         !$omp parallel do default(shared) &
+         !$omp private(n_theta,n_r,n_theta_nhs,n_phi) &
+         !$omp private(vflr2,valr,valr2,vflh2,valh2,valh2m) &
+         !$omp reduction(max:vr2max,vh2max)
+         do n_phi=1,n_phi_max
+            do n_r=n_r_l,n_r_u
+               if ( l_cour_alf_damp ) then
+                  valri2=(half*(one+opm))**2/delxr2(n_r)
+                  valhi2=(half*(one+opm))**2/delxh2(n_r)
+               else
+                  valri2=0.0_cp
+                  valhi2=0.0_cp
+               end if
+               do n_theta=1,n_theta_max
+                  n_theta_nhs=(n_theta+1)/2 ! northern hemisphere=odd n_theta
+
+                  vflr2=orho2(n_r)*vr(n_theta,n_r,n_phi)*vr(n_theta,n_r,n_phi)
+                  valr =br(n_theta,n_r,n_phi)*br(n_theta,n_r,n_phi)*LFfac*orho1(n_r)
+                  valr2=valr*valr/(valr+valri2)
+                  vr2max(n_r)=max(vr2max(n_r),or4(n_r)*(cf2*vflr2+af2*valr2))
+
+                  vflh2= ( vt(n_theta,n_r,n_phi)*vt(n_theta,n_r,n_phi) +  &
+                  &        vp(n_theta,n_r,n_phi)*vp(n_theta,n_r,n_phi) )* &
+                  &        osn2(n_theta_nhs)*orho2(n_r)
+                  valh2= ( bt(n_theta,n_r,n_phi)*bt(n_theta,n_r,n_phi) +  &
+                  &        bp(n_theta,n_r,n_phi)*bp(n_theta,n_r,n_phi) )* &
+                  &        LFfac*osn2(n_theta_nhs)*orho1(n_r)
+                  valh2m=valh2*valh2/(valh2+valhi2)
+                  vh2max(n_r)=max(vh2max(n_r),or2(n_r)*(cf2*vflh2+af2*valh2m))
+
+               end do
+            end do
+         end do
+         !$omp end parallel do
+
+      else   ! Magnetic field ?
+
+         !$omp parallel do default(shared) &
+         !$omp private(n_theta,n_r,n_theta_nhs,n_phi,vflr2,vflh2) &
+         !$omp reduction(max:vr2max,vh2max)
+         do n_phi=1,n_phi_max
+            do n_r=n_r_l,n_r_u
+               do n_theta=1,n_theta_max
+                  n_theta_nhs=(n_theta+1)/2 ! northern hemisphere=odd n_theta
+
+                  vflr2=orho2(n_r)*vr(n_theta,n_r,n_phi)*vr(n_theta,n_r,n_phi)
+                  vr2max(n_r)=max(vr2max(n_r),cf2*or4(n_r)*vflr2)
+
+                  vflh2= ( vt(n_theta,n_r,n_phi)*vt(n_theta,n_r,n_phi) +  &
+                  &        vp(n_theta,n_r,n_phi)*vp(n_theta,n_r,n_phi) )* &
+                  &        osn2(n_theta_nhs)*orho2(n_r)
+                  vh2max(n_r)=max(vh2max(n_r),cf2*or2(n_r)*vflh2)
+
+               end do
+            end do
+         end do
+         !$omp end parallel do
+      end if   ! Magnetic field ?
+
+      !$omp critical
+      do n_r=n_r_l,n_r_u
+         if ( vr2max(n_r) /= 0.0_cp ) then
+            dtrkc(n_r)=min(dtrkc(n_r),sqrt(delxr2(n_r)/vr2max(n_r)))
+         end if
+         if ( vh2max(n_r) /= 0.0_cp ) then
+            dthkc(n_r)=min(dthkc(n_r),sqrt(delxh2(n_r)/vh2max(n_r)))
+         end if
+      end do
+      !$omp end critical
+
+   end subroutine courant_batch
 !------------------------------------------------------------------------------
    subroutine dt_courant(l_new_dt,dt,dt_new,dtMax,dtrkc,dthkc,time)
       !
