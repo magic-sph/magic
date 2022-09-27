429d2ae2e3480ac6a4bb5cd40c46c8f44a6edcb5
rdiarra <rokiatou.diarra@eolen.com>
2022-09-27 11:03:59 +0200

    Add missing modules to load

--- compile.sh
+++ compile.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+module load craype/2.7.17 PrgEnv-cray/8.3.3
+
 module load perftools-base/21.12.0 cce/14.0.2 craype-network-ofi craype-x86-rome cray-fftw/3.3.8.12 cray-libsci/22.06.1.1 libfabric/1.13.1 craype-accel-amd-gfx908 rocm/4.5.0 cray-mpich/8.1.13 gdb4hpc/4.14.2 perftools
 
 export FC=ftn && export CRAY_ACC_DEBUG=1
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/rocm-5.0.2/hip/lib/:/opt/rocm-4.5.0/lib/
+
+if [ -e "build" ];then rm -rf "build" ; fi
 
 mkdir -p build
 
