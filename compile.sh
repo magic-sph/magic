37ef5062333a81a4180ae8b906f1c092a2e70b4a
rdiarra <rokiatou.diarra@eolen.com>
2022-09-27 11:23:06 +0200

    Fix samples running error

--- compile.sh
+++ compile.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+module purge
+
 module load craype/2.7.17 PrgEnv-cray/8.3.3
 
 module load perftools-base/21.12.0 cce/14.0.2 craype-network-ofi craype-x86-rome cray-fftw/3.3.8.12 cray-libsci/22.06.1.1 libfabric/1.13.1 craype-accel-amd-gfx908 rocm/4.5.0 cray-mpich/8.1.13 gdb4hpc/4.14.2 perftools
@@ -18,21 +20,17 @@ cmake .. -DUSE_SHTNS=yes -DUSE_GPU=yes
 
 make -j16
 
-cp magic.exe ../samples/boussBenchSat
-
-cp magic.exe ../samples/dynamo_benchmark
-
 cd ../samples/boussBenchSat
-
-sbatch submitScript.sh
-
 chmod +x clear.sh
+./clear.sh
+cp ../../build/magic.exe .
+sbatch submitScript.sh
 
 cd ../dynamo_benchmark
-
-sbatch submitScript.sh
-
 chmod +x clear.sh
+./clear.sh
+cp ../../build/magic.exe .
+sbatch submitScript.sh
 
 cd ../boussBenchSat
 
