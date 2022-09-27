5096795d19e94390d3a9e99e0a5fb2fe4405465a
rdiarra <rokiatou.diarra@eolen.com>
2022-09-27 10:46:26 +0200

    Add a compilation script : create build dir, compile and then run boussBenchSat & dynamo_benchmark samples

---
+++ compile.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+module load perftools-base/21.12.0 cce/14.0.2 craype-network-ofi craype-x86-rome cray-fftw/3.3.8.12 cray-libsci/22.06.1.1 libfabric/1.13.1 craype-accel-amd-gfx908 rocm/4.5.0 cray-mpich/8.1.13 gdb4hpc/4.14.2 perftools
+
+export FC=ftn && export CRAY_ACC_DEBUG=1
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/rocm-5.0.2/hip/lib/:/opt/rocm-4.5.0/lib/
+
+mkdir -p build
+
+cd build
+
+cmake .. -DUSE_SHTNS=yes -DUSE_GPU=yes
+
+make -j16
+
+cp magic.exe ../samples/boussBenchSat
+
+cp magic.exe ../samples/dynamo_benchmark
+
+cd ../samples/boussBenchSat
+
+sbatch submit.sh
+
+cd ../dynamo_benchmark
+
+sbatch submit.sh
+
+cd ../boussBenchSat
