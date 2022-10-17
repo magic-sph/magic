ae5dbf05125ad23cb234ec06e9fa25e55997f10d
rdiarra <rokiatou.diarra@eolen.com>
2022-10-17 10:54:24 +0200

    Keep loop on nR_out=rscheme_oc%n_max+1,n_r_max on CPU in updateWP.f90 for get_*Mat routines

--- src/updateWP.f90
+++ src/updateWP.f90
@@ -2815,7 +2815,7 @@ contains
 
       if ( rscheme_oc%n_max < n_r_max ) then ! fill with zeros !
 #ifdef WITH_OMP_GPU
-         !$omp target teams distribute parallel do
+         !$omp target
 #endif
          do nR_out=rscheme_oc%n_max+1,n_r_max
             nR_out_p=nR_out+n_r_max
@@ -2829,7 +2829,7 @@ contains
             wpMat%dat(2*n_r_max,nR_out_p)=0.0_cp
          end do
 #ifdef WITH_OMP_GPU
-         !$omp end target teams distribute parallel do
+         !$omp end target
 #endif
       end if
 
@@ -3134,25 +3134,19 @@ contains
          end if
       end if
 
-#ifdef WITH_OMP_GPU
-      !$omp target enter data map(alloc: dat)
-      !$omp target update to(dat)
-#endif
-
       if ( rscheme_oc%n_max < n_r_max ) then ! fill with zeros !
-#ifdef WITH_OMP_GPU
-         !$omp target teams distribute parallel do
-#endif
          do nR_out=rscheme_oc%n_max+1,n_r_max
             dat(1,nR_out)        =0.0_cp
             dat(2,nR_out)        =0.0_cp
             dat(n_r_max-1,nR_out)=0.0_cp
             dat(n_r_max,nR_out)  =0.0_cp
          end do
-#ifdef WITH_OMP_GPU
-         !$omp end target teams distribute parallel do
-#endif
       end if
+
+#ifdef WITH_OMP_GPU
+      !$omp target enter data map(alloc: dat)
+      !$omp target update to(dat)
+#endif
 
       !----- Bulk points:
 #ifdef WITH_OMP_GPU
