bc36beb3f40cda7e582751f5f4107c10dc29d4bc
Thomas Gastine <gastine@mps.mpg.de>
2022-10-21 10:13:23 +0200

    add missing private OMP variable in get_tor_rhs_imp_ghost

--- src/updateZ.f90
+++ src/updateZ.f90
@@ -1180,7 +1180,7 @@ contains
          end do
       end if ! l=1,m=1 contained in lm-block ?
 
-      !$omp parallel default(shared) private(start_lm, stop_lm, n_r, lm, l, m, dL)
+      !$omp parallel default(shared) private(start_lm, stop_lm, n_r, lm, l, m, dL, Dif)
       start_lm=1; stop_lm=lm_max
       call get_openmp_blocks(start_lm,stop_lm)
 
