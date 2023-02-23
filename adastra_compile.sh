0ae3ab749719a58c49bbbd75b353d4408255dcee
rdiarra <rokiatou.diarra@eolen.com>
2023-02-23 12:11:12 +0100

    Add scrips for ADASTRA & Lumi02 env initialization

--- adastra_compile.sh
+++ adastra_compile.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 
-### Env for Lumi02
+### Env for ADASTRA
+
 module purge
 module load craype/2.7.19 PrgEnv-cray/8.3.3
-module load craype-x86-rome libfabric/1.13.1 craype-network-ofi cray-mrnet/5.0.4 cray-mpich/8.1.21
-module load cce/15.0.0 craype-accel-amd-gfx90a cray-fftw/3.3.10.1 cray-libsci/21.08.1.2 rocm/5.2.0
+module load craype-x86-trento libfabric/1.15.2.0 craype-network-ofi cray-dsmml/0.2.2 cray-mpich/8.1.21
+module load cpe/22.11 craype-accel-amd-gfx90a cray-fftw/3.3.10.2 cray-libsci/22.11.1.2 rocm/5.2.3
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/cray/pe/mpich/8.1.21/gtl/lib/
-export LDFLAGS+="-L/opt/cray/pe/fftw/3.3.10.1/x86_rome/lib/"
+export LDFLAGS+="-L/opt/cray/pe/fftw/3.3.10.2/x86_trento/lib/"
 export HIP_LAUNCH_BLOCKING=1
 export AMD_LOG_LEVEL=1
 export OMP_NUM_THREADS=1
 export FC=ftn
 export CRAY_ACC_DEBUG=0
 
-module use -a /common/magic/modulefiles/
+module use -a ${SHAREDHOMEDIR}/modulefiles
+module load rocm/5.2.3
 module load hipfort
-export HIPFORT_PATH=/common/magic/INSTALL/hipfort/0.4-6f6ae98e/cpe-cray/15.0.0/rocm/5.2.0/gfx90a/cmpich/8.1.21
+export HIPFORT_PATH=/lus/home/NAT/cpa2204/SHARED/INSTALL/hipfort/0.4-6f6ae98e/cpe-cray/15.0.0/rocm/5.2.3/cmpich/8.1.21
 
 ### Create a build directory
 if [ -e "build" ];then rm -rf "build" ; fi
@@ -32,7 +33,7 @@ cd ../samples/boussBenchSat
 chmod +x clear.sh
 ./clear.sh
 cp ../../build/magic.exe .
-sbatch submit_lumi02.sh
+sbatch submit_adastra.sh
 
 squeue
 
