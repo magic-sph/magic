1d21dcb1f7394a008cab562bcfc6f8c681fedc50
Thomas Gastine <gastine@ipgp.fr>
2023-04-04 10:05:33 +0200

    clean resub.py

--- submitscripts/resub.py
+++ submitscripts/resub.py
@@ -23,7 +23,7 @@ else:
     n_resub = 1
 
 def get_old_tag(pbs_file):
-    tag = check_output("grep -oP 'tag\s+=\s?+\K(.*),' %s" % pbs_file, shell=True)
+    tag = check_output("grep -oP 'tag\s+=\s?+\K(.*),' {}".format(pbs_file), shell=True)
     tag = tag.decode('utf8').strip('\n')
     tag = tag.split('"')[1] # Get rid of the ""
 
@@ -31,7 +31,7 @@ def get_old_tag(pbs_file):
 
 
 def submit_job(dep, jobfile):
-    job_sub = check_output("sbatch --dependency=afterok:%i %s" % (dep, jobfile),
+    job_sub = check_output("sbatch --dependency=afterok:{:d} {}".format(dep, jobfile),
                            shell=True)
     job_sub = job_sub.decode('utf8').strip('\n')
     job_sub = int(job_sub.split()[-1])
@@ -40,7 +40,7 @@ def submit_job(dep, jobfile):
 
 
 def get_job_dir(jobname):
-    run_path = check_output("scontrol show jobid %i | grep -oP 'WorkDir=\K(.*)'" % jobname,
+    run_path = check_output("scontrol show jobid {:d} | grep -oP 'WorkDir=\K(.*)'".format(jobname),
                             shell=True)
     run_path = run_path.decode('utf8').strip('\n')
     run_path = os.path.realpath(run_path)
@@ -55,7 +55,7 @@ def get_job_dir(jobname):
 
 def get_job_file(jobname, path):
 
-    cmd = check_output("scontrol show jobid %i | grep -oP 'Command=%s\K(.*)'" % (jobname, path),
+    cmd = check_output("scontrol show jobid {:d} | grep -oP 'Command={}\K(.*)'".format(jobname, path),
                             shell=True)
     cmd = cmd.decode('utf8').strip('\n')[1:]
 
@@ -65,7 +65,7 @@ integers = re.compile('[0-9]')
 batchFiles = re.compile(r'run([0-9]+)\.sbatch')
 tagName = re.compile(r' *tag *= *"([\w\.]*)" *, *')
     
-cmd = "squeue -u %s | awk '{print $1}' > list_jobs" % uname
+cmd = "squeue -u {}".format(uname) + " | awk '{print $1}' > list_jobs"
 os.system(cmd)
 jobs = np.loadtxt('list_jobs', skiprows=1)
 jobs = np.sort(jobs)
@@ -86,7 +86,7 @@ for i in range(n_resub):
     # Determine new job file
     if batchFiles.match(old_job):
         oldindex = int(batchFiles.match(old_job).group(1))
-        new_job = 'run%i.sbatch' % (oldindex+1)
+        new_job = 'run{:d}.sbatch'.format(oldindex+1)
     
     # Determine the new tag
     old_tag = get_old_tag(old_job)
@@ -97,22 +97,28 @@ for i in range(n_resub):
         new_tag = old_tag[:-1] + string.ascii_letters[ind+1]
     
     # Determine the new checkpoint file
-    new_checkpoint = 'checkpoint_end.%s' % old_tag
+    new_checkpoint = 'checkpoint_end.{}'.format(old_tag)
     
     # Now create the job file
-    cmd = 'cp %s %s' % (old_job, new_job)
+    cmd = 'cp {} {}'.format(old_job, new_job)
     os.system(cmd)
     
     # SED substitutions
-    cmd = "sed -i 's/ *tag *=.*/tag         =\"%s\",/g' %s" % (new_tag, new_job)
+    cmd = "sed -i 's/ *tag *=.*/tag         =\"{}\",/g' {}".format(new_tag, new_job)
     os.system(cmd)
-    cmd = "sed -i 's/ *start_file.*/start_file  =\"%s\",/g' %s" % (new_checkpoint, new_job)
+    cmd = "sed -i 's/ *start_file.*/start_file  =\"{}\",/g' {}".format(new_checkpoint, new_job)
     os.system(cmd)
-    cmd = "sed -i 's/ *l_start_file.*/l_start_file=.true.,/g' %s" % new_job
+    cmd = "sed -i 's/ *l_start_file.*/l_start_file=.true.,/g' {}".format(new_job)
     os.system(cmd)
-    cmd = "sed -i 's/ *init_u.*/init_u     =0,/g' %s" % new_job
+    cmd = "sed -i 's/ *init_u.*/init_u     =0,/g' {}".format(new_job)
     os.system(cmd)
-    cmd = "sed -i 's/ *init_t.*/init_t     =0,/g' %s" % new_job
+    cmd = "sed -i 's/ *init_s1.*/init_s1     =0,/g' {}".format(new_job)
+    os.system(cmd)
+    cmd = "sed -i 's/ *init_s2.*/init_s2     =0,/g' {}".format(new_job)
+    os.system(cmd)
+    cmd = "sed -i 's/ *init_xi1.*/init_xi1    =0,/g' {}".format(new_job)
+    os.system(cmd)
+    cmd = "sed -i 's/ *init_xi2.*/init_xi2    =0,/g' {}".format(new_job)
     os.system(cmd)
     
     # Now submit the new job and overwrite old by new
