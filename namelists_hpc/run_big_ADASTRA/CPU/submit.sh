7cbd2e2bee8d24b60ff45c036a2da759ec174551
rdiarra <rokiatou.diarra@eolen.com>
2023-06-06 10:40:26 +0200

    Fix ADASTRA compilation / execution issues

--- namelists_hpc/run_big_ADASTRA/CPU/submit.sh
+++ namelists_hpc/run_big_ADASTRA/CPU/submit.sh
@@ -1,35 +1,21 @@
 #!/bin/sh
 
 #SBATCH -J MagIC
-
 #SBATCH --nodes=8
-
 #SBATCH --cpus-per-task=8    # Thread OpenMP per MPI
-
 #SBATCH --ntasks=192           # Total number of MPI
-
 #SBATCH --ntasks-per-node=24
-
 #SBATCH --threads-per-core=1
-
 #SBATCH --time=00:50:00
-
 #SBATCH --exclusive
-
 #SBATCH --constraint=GENOA
-
 #SBATCH -A cpa
-
-#SBATCH --reservation=eolen_cpu
-
-##SBATCH --kill-on-bad-exit
-
 #SBATCH --output=Magic_8N192MPI8OMP_%j.out
 
 module purge
 module load craype/2.7.19 PrgEnv-cray/8.3.3
-module load craype-x86-genoa libfabric/1.15.2.0 craype-network-ofi cray-dsmml/0.2.2 cray-mpich/8.1.21
-module load cpe/22.11 cray-fftw/3.3.10.3 cray-libsci/22.11.1.2
+module load craype-x86-genoa libfabric/1.15.2.0 craype-network-ofi cray-dsmml/0.2.2 cray-mpich/8.1.24
+module load cpe/23.02 cray-fftw/3.3.10.3 cray-libsci/23.02.1.1
 module list
 
 export KMP_STACKSIZE=2g
