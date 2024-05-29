c1e20a4508baec67176e5de69199841241574138
Thomas Gastine <gastine@mps.mpg.de>
2024-05-29 14:07:19 +0200

    remove deprecated 'from __future__ import print_function'

--- samples/testCoeffOutputs/unitTest.py
+++ samples/testCoeffOutputs/unitTest.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 import unittest
 import numpy as np
 import glob
@@ -47,66 +46,66 @@ def generateEkinFile(fileName='e_kin.test'):
 
     # Classical CMB file
     cmb = MagicCoeffCmb(tag='start', iplot=False, quiet=True)
-    st = '%.5e %.5e %.5e %.5e %.5e' % ( cmb.glm[0, cmb.idx[1, 0]], 
-                                        cmb.glm[2, cmb.idx[1, 0]], 
-                                        cmb.hlm[-1, cmb.idx[5, 0]], 
-                                        cmb.glm[2, cmb.idx[5, 4]], 
+    st = '%.5e %.5e %.5e %.5e %.5e' % ( cmb.glm[0, cmb.idx[1, 0]],
+                                        cmb.glm[2, cmb.idx[1, 0]],
+                                        cmb.hlm[-1, cmb.idx[5, 0]],
+                                        cmb.glm[2, cmb.idx[5, 4]],
                                         cmb.hlm[3, cmb.idx[5, 4]] )
     file.write(st+'\n')
 
     # Secular variation CMB file
     cmb = MagicCoeffCmb(tag='start', iplot=False, sv=True, quiet=True)
-    st = '%.5e %.5e %.5e %.5e %.5e' % ( cmb.glm[0, cmb.idx[1, 0]], 
-                                        cmb.glm[2, cmb.idx[1, 0]], 
-                                        cmb.hlm[-1, cmb.idx[5, 0]], 
-                                        cmb.glm[2, cmb.idx[5, 4]], 
+    st = '%.5e %.5e %.5e %.5e %.5e' % ( cmb.glm[1, cmb.idx[1, 0]],
+                                        cmb.glm[2, cmb.idx[1, 0]],
+                                        cmb.hlm[-1, cmb.idx[5, 0]],
+                                        cmb.glm[2, cmb.idx[5, 4]],
                                         cmb.hlm[3, cmb.idx[5, 4]] )
     file.write(st+'\n')
 
     # Time-averaged CMB file
     cmb = MagicCoeffCmb(tag='start',  iplot=False, ave=True, quiet=True)
-    st = '%.5e %.5e %.5e %.5e' % ( cmb.glm[0, cmb.idx[1, 0]], 
-                                   cmb.hlm[0, cmb.idx[5, 0]], 
-                                   cmb.glm[0, cmb.idx[5, 4]], 
+    st = '%.5e %.5e %.5e %.5e' % ( cmb.glm[0, cmb.idx[1, 0]],
+                                   cmb.hlm[0, cmb.idx[5, 0]],
+                                   cmb.glm[0, cmb.idx[5, 4]],
                                    cmb.hlm[0, cmb.idx[5, 4]] )
     file.write(st+'\n')
 
     # Coeff at depth
     coeff = MagicCoeffR(tag='start',  field='V', r=1, iplot=False, quiet=True)
-    st = '%.5e %.5e %.5e %.5e' % ( coeff.wlm[0, coeff.idx[4, 4]].real, 
-                                   coeff.wlm[0, coeff.idx[4, 4]].imag, 
-                                   coeff.zlm[0, coeff.idx[1, 0]].real, 
+    st = '%.5e %.5e %.5e %.5e' % ( coeff.wlm[0, coeff.idx[4, 4]].real,
+                                   coeff.wlm[0, coeff.idx[4, 4]].imag,
+                                   coeff.zlm[0, coeff.idx[1, 0]].real,
                                    coeff.wlm[0, coeff.idx[2, 0]].imag )
     file.write(st+'\n')
 
     coeff = MagicCoeffR(tag='start',  field='V', r=2, iplot=False, quiet=True)
-    st = '%.5e %.5e %.5e %.5e' % ( coeff.wlm[0, coeff.idx[4, 4]].real, 
-                                   coeff.wlm[0, coeff.idx[4, 4]].imag, 
-                                   coeff.zlm[0, coeff.idx[1, 0]].real, 
+    st = '%.5e %.5e %.5e %.5e' % ( coeff.wlm[0, coeff.idx[4, 4]].real,
+                                   coeff.wlm[0, coeff.idx[4, 4]].imag,
+                                   coeff.zlm[0, coeff.idx[1, 0]].real,
                                    coeff.wlm[0, coeff.idx[2, 0]].imag )
     file.write(st+'\n')
 
     coeff = MagicCoeffR(tag='start',  field='B', r=1, iplot=False, quiet=True)
-    st = '%.5e %.5e %.5e %.5e' % ( coeff.wlm[0, coeff.idx[1, 0]].real, 
-                                   coeff.wlm[0, coeff.idx[1, 0]].imag, 
-                                   coeff.zlm[0, coeff.idx[5, 0]].real, 
+    st = '%.5e %.5e %.5e %.5e' % ( coeff.wlm[0, coeff.idx[1, 0]].real,
+                                   coeff.wlm[0, coeff.idx[1, 0]].imag,
+                                   coeff.zlm[0, coeff.idx[5, 0]].real,
                                    coeff.wlm[0, coeff.idx[5, 4]].imag )
     file.write(st+'\n')
 
     coeff = MagicCoeffR(tag='start',  field='B', r=2, iplot=False, quiet=True)
-    st = '%.5e %.5e %.5e %.5e' % ( coeff.wlm[0, coeff.idx[1, 0]].real, 
-                                   coeff.wlm[0, coeff.idx[1, 0]].imag, 
-                                   coeff.zlm[0, coeff.idx[4, 0]].real, 
+    st = '%.5e %.5e %.5e %.5e' % ( coeff.wlm[0, coeff.idx[1, 0]].real,
+                                   coeff.wlm[0, coeff.idx[1, 0]].imag,
+                                   coeff.zlm[0, coeff.idx[4, 0]].real,
                                    coeff.wlm[0, coeff.idx[5, 4]].imag )
     file.write(st+'\n')
 
     pot = MagicPotential(field='B', verbose=False)
-    st = '%.5e %.5e %.5e %.5e' % ( pot.pol[5, 3].real, pot.pol[127, 15].imag, 
+    st = '%.5e %.5e %.5e %.5e' % ( pot.pol[5, 3].real, pot.pol[127, 15].imag,
                                    pot.tor[183, 16].real, pot.tor[121, 9].imag )
     file.write(st+'\n')
 
     pot = MagicPotential(field='V', verbose=False)
-    st = '%.5e %.5e %.5e %.5e' % ( pot.pol[65, 15].real, pot.pol[125, 17].imag, 
+    st = '%.5e %.5e %.5e %.5e' % ( pot.pol[65, 15].real, pot.pol[125, 17].imag,
                                    pot.tor[63, 12].real, pot.tor[184, 13].imag )
     file.write(st+'\n')
 
